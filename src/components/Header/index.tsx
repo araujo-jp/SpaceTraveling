756f31fd6257c26b9065473e4f32cad27bf730e4
Joao Paulo Araujo <paulointer077@gmail.com>
2021-09-28 02:41:50 -0400

    fix: correcting logo display error on post page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ export default function Header() {
       <div className={styles.headerContent}>
         <Link href="/">
           <a>
-            <img src="logo.svg" alt="logo" />
+            <img src="/logo.svg" alt="logo" />
           </a>
         </Link>
       </div>
